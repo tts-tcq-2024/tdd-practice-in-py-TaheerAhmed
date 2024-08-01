12f9422b5214fac6d93e2e66a9853ad90b91022a
TaheerAhmed <85991537+TaheerAhmed@users.noreply.github.com>
2024-08-02 03:29:53 +0530

    Updated StringCalculatorTest.py

--- StringCalculatorTest.py
+++ StringCalculatorTest.py
@@ -9,22 +9,26 @@ class TestStringCalculator(unittest.TestCase):
                 self.assertEqual(add("0"), 0)
                 
         def test_expectSumForTwoNumberst(self):
-                self.assertEqual(add("1,2"), 3)
+                self.assertEqual(add("1;2"), 3)
+                self.assertEqual(add("25;25"),50)
+                self.assertEqual(add("4\n5"),9)
                 
         def test_ignoreNumbersGreaterThan1000(self):
-                self.assertEqual(add("1,1001"), 1)
+                self.assertEqual(add("1;1001"), 1)
+                self.assertEqual(add("2000;1;4"),5)
                 
         def test_expectSumWithCustomDelimiter(self):
                 self.assertEqual(add("//;\n1;2"), 3)
                 
         def test_expectSumWithNewlineDelimiter(self):
-                self.assertEqual(add("1\n2,3"),6);
-        
+                self.assertEqual(add("1\n2;3"),6);
+                self.assertEqual(add("3\n4\n3"),10)
+        def test_negative_numbers(self):
+                with self.assertRaises(ValueError):
+                        add("-1,0,-3")
 
 
 
 
 if __name__ == '__main__':
     unittest.main()
-
-
